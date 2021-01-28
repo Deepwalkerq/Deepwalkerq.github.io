fed95c325ce3e801b78fb2b1ef899f0c721de1bd
Deepwalker <nelsen278@gmail.com>
2021-01-28 14:50:56 +0300

    test branch

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
 console.log("Nekogatari")
 
-let mainTab = document.querySelector(".main")
-let memberTab = document.querySelector(".members")
-
-
-if (document.cookie == ""){
-    document.cookie = "active=main"
-}
-
-// function checkTab(e, cookie){
-// }
-
-function openTab(e, tabName){
-
-    if (tabName == "main"){
-        mainTab.style.display = "flex"
-        memberTab.style.display = "none"
+window.onload = () => {
+    if (sessionStorage.length == 0){
+        sessionStorage.setItem("activeTab", "main")
+        checkTab()
+    } else{
+        checkTab()
     }
-    else {
-        mainTab.style.display = "none"
-        memberTab.style.display = "flex"
-    }
-
     
 }
+
+const mainTab = document.querySelector(".main")
+const memberTab = document.querySelector(".members")
+const mainLink = document.getElementById("tablink-main")
+const membersLink = document.getElementById("tablink-members")
+
+function checkTab() {
+    if (sessionStorage.getItem("activeTab") == "main"){
+        mainTab.style.display = "flex"
+        memberTab.style.display = "none"
+    } else {
+        mainTab.style.display = "none"
+        memberTab.style.display = "flex"
+
+    }
+
+}
+
+mainLink.addEventListener("click", function() {
+    sessionStorage.setItem("activeTab", "main")
+    checkTab()
+})
+
+membersLink.addEventListener("click", function() {
+    sessionStorage.setItem("activeTab", "members")
+    checkTab()
+})
